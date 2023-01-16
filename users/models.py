c4677c894827217d57ac1cc6a526758223b2cffe
Zain <muhzainjaved@gmail.com>
2023-01-16 15:03:09 +0500

    Created a project using graphene django with graphene auth to register and authenticate users

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class ExtendedUser(AbstractUser):
+
+    email = models.EmailField(blank=False,max_length=255,verbose_name='email')
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
